45c1d5b767c5a3385b473aebad865583f213d04c
AugustoUCE <aysalazar@uce.edu.ec>
2026-02-02 11:46:43 -0500

    01multinodo

---
+++ app-authors/src/main/java/uce/edu/ec/package-info.java
@@ -0,0 +1,2 @@
+
+package uce.edu.ec;
---
+++ app-customers/src/test/java/uce/edu/ec/AppCustomersTest.java
@@ -0,0 +1,21 @@
+package uce.edu.ec;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class AppCustomersTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
