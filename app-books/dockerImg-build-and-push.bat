0a646f7fe2404e74dc1633a476b4a96f6c64e514
AugustoUCE <aysalazar@uce.edu.ec>
2026-02-08 00:43:35 -0500

    dockercompose

---
+++ app-books/dockerImg-build-and-push.bat
@@ -0,0 +1,29 @@
+@echo off
+set DOCKER_USER=augustouce
+set IMAGE=app-books-tg
+set VERSION=latest
+
+echo ===============================
+echo Construyendo imagen Docker
+echo ===============================
+docker build -t %DOCKER_USER%/%IMAGE%:%VERSION% .
+
+if %ERRORLEVEL% neq 0 (
+  echo ❌ Error al construir la imagen
+  pause
+  exit /b 1
+)
+
+echo ===============================
+echo Subiendo imagen a Docker Hub
+echo ===============================
+docker push %DOCKER_USER%/%IMAGE%:%VERSION%
+
+if %ERRORLEVEL% neq 0 (
+  echo ❌ Error al hacer push
+  pause
+  exit /b 1
+)
+
+echo ✅ Imagen subida correctamente a Docker Hub
+pause
