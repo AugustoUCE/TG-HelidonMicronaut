5a907f36a2446e76067fb0d7dbb5255aa07e64f0
AugustoUCE <aysalazar@uce.edu.ec>
2026-02-08 09:37:28 -0500

    metricas

--- app-authors/src/main/resources/db/migration/V1.0.3__crear-tabla-book-author.sql .sql
+++ app-authors/src/main/resources/db/migration/V1.0.3__crear-tabla-book-author.sql .sql
@@ -8,3 +8,8 @@ CREATE TABLE book_author
     CONSTRAINT fk_book_author_book
         FOREIGN KEY (books_isbn) REFERENCES book (isbn)
 );
+
+INSERT INTO book_author (books_isbn, authors_id) VALUES
+    ('978-3-16-148410-0', 1),
+    ('978-0-14-303995-2', 2),
+    ('978-0-14243703-2', 3);
